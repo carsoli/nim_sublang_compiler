169ff70c776a40b611bccbb9fb157083a7aae4c3
carsoli <caroleemad96@gmail.com>
2019-04-05 16:55:57 +0200

    w errors

--- milestone_1.py
+++ milestone_1.py
@@ -1,21 +1,20 @@
-import os
+import sys, os
 import argparse
-from utils import read_file, write_to_file, tokenize_file
+from utils import write_to_file, tokenize_file
 
 def main():
-    grammar_path = args.grammar
-    tokens = tokenize_file(grammar_path)
-    
+    testfile_path = args.file
+    tokens = tokenize_file(testfile_path)
+    print(tokens)
     res_filename = sys.modules[__name__].__file__.rpartition(".")[0] + "_result.txt" 
-    write_to_file(res_filename, "\n".join(tokens) )
-
+    # write_to_file(res_filename, "\n".join(tokens) )
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(add_help=True, description='command-line args parser')
 
-    parser.add_argument('--grammar', action="store", help="path to file that contains rule definitons", nargs="?", 
-    metavar="grammar_file")
+    parser.add_argument('--file', action="store", help="path to file that contains input test grammar", nargs="?", 
+    metavar="file")
     
     args = parser.parse_args()
 
--- milestone_1Parser.py
+++ milestone_1Parser.py
@@ -8,9 +8,9 @@ import sys
 
 def serializedATN():
     with StringIO() as buf:
-        buf.write("\3\u608b\ua72a\u8133\ub9ed\u417c\u3be7\u7786\u5964\3\u0094")
+        buf.write("\3\u608b\ua72a\u8133\ub9ed\u417c\u3be7\u7786\u5964\3\u0095")
         buf.write("\7\4\2\t\2\3\2\3\2\3\2\2\2\3\2\2\2\2\5\2\4\3\2\2\2\4\5")
-        buf.write("\7\31\2\2\5\3\3\2\2\2\2")
+        buf.write("\7\32\2\2\5\3\3\2\2\2\2")
         return buf.getvalue()
 
 
@@ -25,57 +25,58 @@ class milestone_1Parser ( Parser ):
     sharedContextCache = PredictionContextCache()
 
     literalNames = [ "<INVALID>", "'#'", "<INVALID>", "<INVALID>", "<INVALID>", 
-                     "'\r\n'", "' '", "<INVALID>", "','", "';'", "':'", 
-                     "'.'", "'('", "')'", "'{'", "'}'", "'['", "']'", "'interface'", 
-                     "'enum'", "'object'", "'concept'", "'type'", "'and'", 
-                     "'or'", "'xor'", "'shl'", "'shr'", "'mod'", "'div'", 
-                     "'@'", "'&'", "'|'", "'+'", "'-'", "'/'", "'<'", "'>'", 
-                     "'%'", "'^'", "<INVALID>", "'*'", "'~'", "'!'", "'var'", 
-                     "'let'", "'const'", "'bind'", "'proc'", "'static'", 
-                     "'method'", "'func'", "'mixin'", "'end'", "'except'", 
-                     "'return'", "'try'", "'raise'", "'finally'", "'out'", 
-                     "'case'", "'else'", "'elif'", "'do'", "'while'", "'iterator'", 
-                     "'for'", "'in'", "'notin'", "'continue'", "'break'", 
-                     "'template'", "'tuple'", "'using'", "'when'", "'yield'", 
-                     "'include'", "'import'", "'from'", "'as'", "'export'", 
-                     "'ref'", "'ptr'", "'addr'", "'is'", "'not'", "'isnot'", 
-                     "'asm'", "'block'", "'macro'", "'nil'", "'cast'", "'converter'", 
-                     "'of'", "'defer'", "'discard'", "'distinct'", "<INVALID>", 
+                     "<INVALID>", "'\r\n'", "' '", "<INVALID>", "','", "';'", 
+                     "':'", "'.'", "'('", "')'", "'{'", "'}'", "'['", "']'", 
+                     "'interface'", "'enum'", "'object'", "'concept'", "'type'", 
+                     "'and'", "'or'", "'xor'", "'shl'", "'shr'", "'mod'", 
+                     "'div'", "'@'", "'&'", "'|'", "'+'", "'-'", "'/'", 
+                     "'<'", "'>'", "'%'", "'^'", "<INVALID>", "'*'", "'~'", 
+                     "'!'", "'var'", "'let'", "'const'", "'bind'", "'proc'", 
+                     "'static'", "'method'", "'func'", "'mixin'", "'end'", 
+                     "'except'", "'return'", "'try'", "'raise'", "'finally'", 
+                     "'out'", "'case'", "'else'", "'elif'", "'do'", "'while'", 
+                     "'iterator'", "'for'", "'in'", "'notin'", "'continue'", 
+                     "'break'", "'template'", "'tuple'", "'using'", "'when'", 
+                     "'yield'", "'include'", "'import'", "'from'", "'as'", 
+                     "'export'", "'ref'", "'ptr'", "'addr'", "'is'", "'not'", 
+                     "'isnot'", "'asm'", "'block'", "'macro'", "'nil'", 
+                     "'cast'", "'converter'", "'of'", "'defer'", "'discard'", 
+                     "'distinct'", "<INVALID>", "<INVALID>", "<INVALID>", 
                      "<INVALID>", "<INVALID>", "<INVALID>", "<INVALID>", 
                      "<INVALID>", "<INVALID>", "<INVALID>", "<INVALID>", 
                      "<INVALID>", "<INVALID>", "<INVALID>", "<INVALID>", 
                      "<INVALID>", "<INVALID>", "<INVALID>", "<INVALID>", 
                      "<INVALID>", "<INVALID>", "<INVALID>", "<INVALID>", 
                      "<INVALID>", "<INVALID>", "<INVALID>", "<INVALID>", 
-                     "<INVALID>", "<INVALID>", "<INVALID>", "'\\\"'", "'''", 
-                     "<INVALID>", "'\\a'", "'\\b'", "'\\'", "'\\e'", "<INVALID>", 
-                     "'\\f'", "<INVALID>", "<INVALID>", "'\\p'" ]
+                     "<INVALID>", "'\\\"'", "'''", "<INVALID>", "'\\a'", 
+                     "'\\b'", "'\\'", "'\\e'", "<INVALID>", "'\\f'", "<INVALID>", 
+                     "<INVALID>", "'\\p'" ]
 
     symbolicNames = [ "<INVALID>", "HASH", "COMMENT_BODY", "COMMENT", "MULTILINE_COMMENT", 
-                      "NEWLINE", "WS", "INDENT", "COMMA", "SEMI_COLON", 
-                      "COLON", "DOT", "OPEN_PAREN", "CLOSE_PAREN", "OPEN_BRACE", 
-                      "CLOSE_BRACE", "OPEN_BRACK", "CLOSE_BRACK", "INTERFACE", 
-                      "ENUM", "OBJECT", "CONCEPT", "TYPE", "AND", "OR", 
-                      "XOR", "SHL", "SHR", "MOD", "DIV", "AT", "AND_OPERATOR", 
-                      "OR_OPERATOR", "ADD_OPERATOR", "MINUS_OPERATOR", "DIV_OPERATOR", 
-                      "LESS_THAN", "GREATER_THAN", "MODULUS", "XOR_OPERATOR", 
-                      "EQUALS_OPERATOR", "MUL_OPERATOR", "BITWISE_NOT_OPERATOR", 
-                      "NOT_OPERATOR", "VARIABLE", "LET", "CONST", "BIND", 
-                      "PROC", "STATIC", "METHOD", "FUNC", "MIXIN", "END", 
-                      "EXCEPT", "RETURN", "TRY", "RAISE", "FINALLY", "OUT", 
-                      "CASE", "ELSE", "ELIF", "DO", "WHILE", "ITERATOR", 
-                      "FOR", "IN", "NOTIN", "CONTINUE", "BREAK", "TEMPLATE", 
-                      "TUPLE", "USING", "WHEN", "YIELD", "INCLUDE", "IMPORT", 
-                      "FROM", "AS", "EXPORT", "REF", "PTR", "ADDR", "IS", 
-                      "NOT", "ISNOT", "ASM", "BLOCK", "MACRO", "NIL", "CAST", 
-                      "CONVERTER", "OF", "DEFER", "DISCARD", "DISTINCT", 
-                      "DIGIT", "LETTER", "IDENTIFIER", "OCTDIGIT", "BINDIGIT", 
-                      "HEXDIGIT", "HEX_LIT", "BIN_LIT", "OCT_LIT", "DEC_LIT", 
-                      "INT_LIT", "INT_SUFFIX", "INT8_LIT", "INT16_LIT", 
-                      "INT32_LIT", "INT64_LIT", "UINT_SUFFIX", "UINT_LIT", 
-                      "UINT8_LIT", "UINT16_LIT", "UINT32_LIT", "UINT64_LIT", 
-                      "EXP", "FLOAT_LIT", "FLOAT32_SUFFIX", "FLOAT32_LIT", 
-                      "FLOAT64_SUFFIX", "FLOAT64_LIT", "QUOTATION_MARK", 
+                      "DOCUMENTATION_COMMENT", "NEWLINE", "WS", "INDENT", 
+                      "COMMA", "SEMI_COLON", "COLON", "DOT", "OPEN_PAREN", 
+                      "CLOSE_PAREN", "OPEN_BRACE", "CLOSE_BRACE", "OPEN_BRACK", 
+                      "CLOSE_BRACK", "INTERFACE", "ENUM", "OBJECT", "CONCEPT", 
+                      "TYPE", "AND", "OR", "XOR", "SHL", "SHR", "MOD", "DIV", 
+                      "AT", "AND_OPERATOR", "OR_OPERATOR", "ADD_OPERATOR", 
+                      "MINUS_OPERATOR", "DIV_OPERATOR", "LESS_THAN", "GREATER_THAN", 
+                      "MODULUS", "XOR_OPERATOR", "EQUALS_OPERATOR", "MUL_OPERATOR", 
+                      "BITWISE_NOT_OPERATOR", "NOT_OPERATOR", "VARIABLE", 
+                      "LET", "CONST", "BIND", "PROC", "STATIC", "METHOD", 
+                      "FUNC", "MIXIN", "END", "EXCEPT", "RETURN", "TRY", 
+                      "RAISE", "FINALLY", "OUT", "CASE", "ELSE", "ELIF", 
+                      "DO", "WHILE", "ITERATOR", "FOR", "IN", "NOTIN", "CONTINUE", 
+                      "BREAK", "TEMPLATE", "TUPLE", "USING", "WHEN", "YIELD", 
+                      "INCLUDE", "IMPORT", "FROM", "AS", "EXPORT", "REF", 
+                      "PTR", "ADDR", "IS", "NOT", "ISNOT", "ASM", "BLOCK", 
+                      "MACRO", "NIL", "CAST", "CONVERTER", "OF", "DEFER", 
+                      "DISCARD", "DISTINCT", "DIGIT", "LETTER", "IDENTIFIER", 
+                      "OCTDIGIT", "BINDIGIT", "HEXDIGIT", "HEX_LIT", "BIN_LIT", 
+                      "OCT_LIT", "DEC_LIT", "INT_LIT", "INT_SUFFIX", "INT8_LIT", 
+                      "INT16_LIT", "INT32_LIT", "INT64_LIT", "UINT_SUFFIX", 
+                      "UINT_LIT", "UINT8_LIT", "UINT16_LIT", "UINT32_LIT", 
+                      "UINT64_LIT", "EXP", "FLOAT_LIT", "FLOAT32_SUFFIX", 
+                      "FLOAT32_LIT", "FLOAT64_SUFFIX", "FLOAT64_LIT", "QUOTATION_MARK", 
                       "APOSTRAPHE", "CARRIAGE_RETURN", "ALERT", "BACKSPACE", 
                       "BACKSLASH", "ESCAPE", "LINE_FEED", "FORM_FEED", "TAB", 
                       "CODE_CHARACTER", "PLATFORM_SPECIFIC_NEWLINE", "CHAR_ESCAPE_SEQUENCES", 
@@ -92,148 +93,149 @@ class milestone_1Parser ( Parser ):
     COMMENT_BODY=2
     COMMENT=3
     MULTILINE_COMMENT=4
-    NEWLINE=5
-    WS=6
-    INDENT=7
-    COMMA=8
-    SEMI_COLON=9
-    COLON=10
-    DOT=11
-    OPEN_PAREN=12
-    CLOSE_PAREN=13
-    OPEN_BRACE=14
-    CLOSE_BRACE=15
-    OPEN_BRACK=16
-    CLOSE_BRACK=17
-    INTERFACE=18
-    ENUM=19
-    OBJECT=20
-    CONCEPT=21
-    TYPE=22
-    AND=23
-    OR=24
-    XOR=25
-    SHL=26
-    SHR=27
-    MOD=28
-    DIV=29
-    AT=30
-    AND_OPERATOR=31
-    OR_OPERATOR=32
-    ADD_OPERATOR=33
-    MINUS_OPERATOR=34
-    DIV_OPERATOR=35
-    LESS_THAN=36
-    GREATER_THAN=37
-    MODULUS=38
-    XOR_OPERATOR=39
-    EQUALS_OPERATOR=40
-    MUL_OPERATOR=41
-    BITWISE_NOT_OPERATOR=42
-    NOT_OPERATOR=43
-    VARIABLE=44
-    LET=45
-    CONST=46
-    BIND=47
-    PROC=48
-    STATIC=49
-    METHOD=50
-    FUNC=51
-    MIXIN=52
-    END=53
-    EXCEPT=54
-    RETURN=55
-    TRY=56
-    RAISE=57
-    FINALLY=58
-    OUT=59
-    CASE=60
-    ELSE=61
-    ELIF=62
-    DO=63
-    WHILE=64
-    ITERATOR=65
-    FOR=66
-    IN=67
-    NOTIN=68
-    CONTINUE=69
-    BREAK=70
-    TEMPLATE=71
-    TUPLE=72
-    USING=73
-    WHEN=74
-    YIELD=75
-    INCLUDE=76
-    IMPORT=77
-    FROM=78
-    AS=79
-    EXPORT=80
-    REF=81
-    PTR=82
-    ADDR=83
-    IS=84
-    NOT=85
-    ISNOT=86
-    ASM=87
-    BLOCK=88
-    MACRO=89
-    NIL=90
-    CAST=91
-    CONVERTER=92
-    OF=93
-    DEFER=94
-    DISCARD=95
-    DISTINCT=96
-    DIGIT=97
-    LETTER=98
-    IDENTIFIER=99
-    OCTDIGIT=100
-    BINDIGIT=101
-    HEXDIGIT=102
-    HEX_LIT=103
-    BIN_LIT=104
-    OCT_LIT=105
-    DEC_LIT=106
-    INT_LIT=107
-    INT_SUFFIX=108
-    INT8_LIT=109
-    INT16_LIT=110
-    INT32_LIT=111
-    INT64_LIT=112
-    UINT_SUFFIX=113
-    UINT_LIT=114
-    UINT8_LIT=115
-    UINT16_LIT=116
-    UINT32_LIT=117
-    UINT64_LIT=118
-    EXP=119
-    FLOAT_LIT=120
-    FLOAT32_SUFFIX=121
-    FLOAT32_LIT=122
-    FLOAT64_SUFFIX=123
-    FLOAT64_LIT=124
-    QUOTATION_MARK=125
-    APOSTRAPHE=126
-    CARRIAGE_RETURN=127
-    ALERT=128
-    BACKSPACE=129
-    BACKSLASH=130
-    ESCAPE=131
-    LINE_FEED=132
-    FORM_FEED=133
-    TAB=134
-    CODE_CHARACTER=135
-    PLATFORM_SPECIFIC_NEWLINE=136
-    CHAR_ESCAPE_SEQUENCES=137
-    CHAR_LIT=138
-    STR_LIT=139
-    STR_LIT_ENDING=140
-    TRIPLESTR_LIT=141
-    RSTR_LIT=142
-    GENERALIZED_RSTR_LIT=143
-    GENERALIZED_STR_LIT=144
-    GENERALIZED_RTRIPSTR_LIT=145
-    GENERALIZED_TRIPLESTR_LIT=146
+    DOCUMENTATION_COMMENT=5
+    NEWLINE=6
+    WS=7
+    INDENT=8
+    COMMA=9
+    SEMI_COLON=10
+    COLON=11
+    DOT=12
+    OPEN_PAREN=13
+    CLOSE_PAREN=14
+    OPEN_BRACE=15
+    CLOSE_BRACE=16
+    OPEN_BRACK=17
+    CLOSE_BRACK=18
+    INTERFACE=19
+    ENUM=20
+    OBJECT=21
+    CONCEPT=22
+    TYPE=23
+    AND=24
+    OR=25
+    XOR=26
+    SHL=27
+    SHR=28
+    MOD=29
+    DIV=30
+    AT=31
+    AND_OPERATOR=32
+    OR_OPERATOR=33
+    ADD_OPERATOR=34
+    MINUS_OPERATOR=35
+    DIV_OPERATOR=36
+    LESS_THAN=37
+    GREATER_THAN=38
+    MODULUS=39
+    XOR_OPERATOR=40
+    EQUALS_OPERATOR=41
+    MUL_OPERATOR=42
+    BITWISE_NOT_OPERATOR=43
+    NOT_OPERATOR=44
+    VARIABLE=45
+    LET=46
+    CONST=47
+    BIND=48
+    PROC=49
+    STATIC=50
+    METHOD=51
+    FUNC=52
+    MIXIN=53
+    END=54
+    EXCEPT=55
+    RETURN=56
+    TRY=57
+    RAISE=58
+    FINALLY=59
+    OUT=60
+    CASE=61
+    ELSE=62
+    ELIF=63
+    DO=64
+    WHILE=65
+    ITERATOR=66
+    FOR=67
+    IN=68
+    NOTIN=69
+    CONTINUE=70
+    BREAK=71
+    TEMPLATE=72
+    TUPLE=73
+    USING=74
+    WHEN=75
+    YIELD=76
+    INCLUDE=77
+    IMPORT=78
+    FROM=79
+    AS=80
+    EXPORT=81
+    REF=82
+    PTR=83
+    ADDR=84
+    IS=85
+    NOT=86
+    ISNOT=87
+    ASM=88
+    BLOCK=89
+    MACRO=90
+    NIL=91
+    CAST=92
+    CONVERTER=93
+    OF=94
+    DEFER=95
+    DISCARD=96
+    DISTINCT=97
+    DIGIT=98
+    LETTER=99
+    IDENTIFIER=100
+    OCTDIGIT=101
+    BINDIGIT=102
+    HEXDIGIT=103
+    HEX_LIT=104
+    BIN_LIT=105
+    OCT_LIT=106
+    DEC_LIT=107
+    INT_LIT=108
+    INT_SUFFIX=109
+    INT8_LIT=110
+    INT16_LIT=111
+    INT32_LIT=112
+    INT64_LIT=113
+    UINT_SUFFIX=114
+    UINT_LIT=115
+    UINT8_LIT=116
+    UINT16_LIT=117
+    UINT32_LIT=118
+    UINT64_LIT=119
+    EXP=120
+    FLOAT_LIT=121
+    FLOAT32_SUFFIX=122
+    FLOAT32_LIT=123
+    FLOAT64_SUFFIX=124
+    FLOAT64_LIT=125
+    QUOTATION_MARK=126
+    APOSTRAPHE=127
+    CARRIAGE_RETURN=128
+    ALERT=129
+    BACKSPACE=130
+    BACKSLASH=131
+    ESCAPE=132
+    LINE_FEED=133
+    FORM_FEED=134
+    TAB=135
+    CODE_CHARACTER=136
+    PLATFORM_SPECIFIC_NEWLINE=137
+    CHAR_ESCAPE_SEQUENCES=138
+    CHAR_LIT=139
+    STR_LIT=140
+    STR_LIT_ENDING=141
+    TRIPLESTR_LIT=142
+    RSTR_LIT=143
+    GENERALIZED_RSTR_LIT=144
+    GENERALIZED_STR_LIT=145
+    GENERALIZED_RTRIPSTR_LIT=146
+    GENERALIZED_TRIPLESTR_LIT=147
 
     def __init__(self, input:TokenStream, output:TextIO = sys.stdout):
         super().__init__(input, output)
