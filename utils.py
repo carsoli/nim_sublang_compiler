41ada7d6a437c0e145ca8068625745678119f83c
Michael <michael.kamel.2020@gmail.com>
2019-04-19 23:01:00 +0200

    visualize with cs

--- utils.py
+++ utils.py
@@ -64,7 +64,19 @@ def dfs_tree(treeview, parent_idx, idx_in_parent, node, ruleNames):
     for i in range(0, node.getChildCount()):
         dfs_tree(treeview, current_idx, i, node.getChild(i), ruleNames)
 
-def visualize_tree(tree, parser):
+def dfs_tree_w_cs(treeview, parent_idx, idx_in_parent, node, ruleNames):
+    current_rule_text = escapeWhitespace(Trees.getNodeText(node, ruleNames), False)
+    current_idx = treeview.insert(parent_idx, idx_in_parent, text=current_rule_text)
+    subTreeText = (current_rule_text if node.getChildCount() == 0 else "")
+    for i in range(0, node.getChildCount()):
+        subTreeText += dfs_tree_w_cs(treeview, current_idx, i+1, node.getChild(i), ruleNames)
+        subTreeText += " "
+    if node.getChildCount() > 0:
+        treeview.insert(current_idx, 0, text=subTreeText)
+    
+    return subTreeText
+
+def visualize_tree(tree, parser, allow_cs=True):
     ruleNames = parser.ruleNames
     tree_gui = Tk()
     tree_gui.geometry("1680x1200+200+200") 
@@ -72,7 +84,10 @@ def visualize_tree(tree, parser):
     ttk.Label(tree_gui, text="Tree View").pack()  
     treeview=ttk.Treeview(tree_gui, height=37, show="tree")
     treeview.pack(fill="both", expand=True)
-    dfs_tree(treeview, '', 'end', tree, ruleNames)
+    if allow_cs:
+        dfs_tree_w_cs(treeview, '', 'end', tree, ruleNames)
+    else:
+        dfs_tree(treeview, '', 'end', tree, ruleNames)
     tree_gui.mainloop()
     
 
